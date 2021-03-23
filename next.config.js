94d9984e271ab2f4138332046375e49ed9350523
kells <kellogbonna@gmail.com>
2021-03-23 19:57:07 +0100

    add image parser

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
     config.module.rules.push({
       test: /\.md$/,
       use: "raw-loader",
+      images: '/posts-images/social-media-header.jpg',
     });
 
     return config;
