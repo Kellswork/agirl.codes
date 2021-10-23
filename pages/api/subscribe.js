14c9b01b1389712e5479d4c45205a6c3f285e144
Kellswork <kellogbonna@gmail.com>
2021-10-23 10:29:42 +0100

    change api response call from promise to async

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export default (req, res) => {
+export default async (req, res) => {
   const { email } = req.body
 
   if (!email || !email.length) {
@@ -23,26 +23,23 @@ export default (req, res) => {
       Authorization: `api_key ${API_KEY}`
     }
   }
-  axios
-    .post(url, data, options)
-    .then((response) => {
-      console.log('res', response)
-      if (response.status >= 400) {
-        return res.status(400).json({
-          error: `There was an error subscribing to the newsletter. Shoot me an email at ogbonnakell@gmail and I'll add you to the list.`
-        })
-      }
-      return res.status(201).json({ message: 'success' })
-    })
-    .catch((error) => {
-      console.log(error)
-      return res.status(500).json({ error: error.message })
-    })
-    
+
+  try {
+    const response = await axios.post(url, data, options)
+    if (response.status >= 400) {
+      return res.status(400).json({
+        error: `There was an error subscribing to the newsletter. Shoot me an email at ogbonnakell@gmail and I'll add you to the list.`
+      })
+    }
+    return res.status(201).json({ message: 'success' })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: error.message })
+  }
 }
 //steps:
-    // get mailchimp api key, audience id,
-    // install axios and setup  post request using Promise, i dunno if async works, 
-    // pass the data need ( data, options and url), make sure not to make any mistakes in the url and data 
-    // infact with any parameter being passed cause it could lead to an error message
-    // move to componnents/subscribe
+// get mailchimp api key, audience id,
+// install axios and setup  post request using async
+// pass the data need ( data, options and url), make sure not to make any mistakes in the url and data
+// infact with any parameter being passed cause it could lead to an error message
+// move to componnents/subscribe
